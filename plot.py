2cc24234121bcd383efe6b60a5803643f2ba3320
Piet Hoffmann <hoffmannpiet42@gmail.com>
2017-11-26 01:46:35 +0100

    durchfuehrung geschrieben, auswertung angefangen

--- plot.py
+++ plot.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+import matplotlib as mpl
+
+mpl.use('pgf')
+mpl.rcParams.update({
+    'pgf.preamble': r'\usepackage{siunitx}',
+})
+
+
 x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
 
